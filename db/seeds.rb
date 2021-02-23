14d5708e0e5d476b0f05d2b7f6ff52a6160ce205
David Rafe <david.rafe@gmail.com>
2021-02-23 16:09:20 -0500

    seeded database with some dummy data for now

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,7 @@
-Faker::Hipster.sentences(number: 1)
+s1 = Seller.create(username: "ilovecats", password: "cats")
+s2 = Seller.create(username: "ilovedogs", password: "dogs")
+s3 = Seller.create(username: "ilovebirds", password: "birds")
+
+a1 = ArtPost.create(title: "hipart", description: "this is the hippist piece ever", price: 50)
+a2 = ArtPost.create(title: "coolart", description: "this is the coolest piece ever", price: 75)
+a3 = ArtPost.create(title: "fancyart", description: "this is the fanciest piece ever", price: 200)
