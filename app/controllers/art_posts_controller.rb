f841b52d83470e9ffe63fef119d80736755fc361
David Rafe <david.rafe@gmail.com>
2021-02-23 17:05:15 -0500

    finished basic version of CRUD routes and views for art_posts

--- app/controllers/art_posts_controller.rb
+++ app/controllers/art_posts_controller.rb
@@ -17,4 +17,21 @@ class ArtPostsController < ApplicationController
         @art_post = ArtPost.find(params[:id])
         erb :'art_posts/show'
     end
+
+    get '/art_posts/:id/edit' do 
+        @art_post = ArtPost.find(params[:id])
+        erb :'art_posts/edit'
+    end
+
+    patch '/art_posts/:id' do 
+        @art_post = ArtPost.find(params[:id])
+        @art_post.update(title: params[:title], description: params[:description], price: params[:price].to_i)
+        redirect "/art_posts/#{@art_post.id}"
+    end
+
+    delete '/art_posts/:id' do
+        @art_post = ArtPost.find(params[:id])
+        @art_post.destroy
+        redirect "/art_posts"
+    end
 end
