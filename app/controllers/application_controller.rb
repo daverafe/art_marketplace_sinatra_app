970d26f640b68927a97714fdece64665aaacf310
David Rafe <david.rafe@gmail.com>
2021-02-24 18:57:56 -0500

    added helper methods current_user and logged_in?, completed basic user authentication in routes and views, refactored art_posts controller with private methods to keep code DRY

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,16 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  helpers do 
+    
+    def current_user 
+     @current_user ||= Seller.find_by_id(session[:seller_id])
+    end
+
+    def logged_in?
+      !!session[:seller_id]
+    end
+
+  end
+
 end
--- app/controllers/art_posts_controller.rb
+++ app/controllers/art_posts_controller.rb
@@ -9,29 +9,47 @@ class ArtPostsController < ApplicationController
     end
 
     post '/art_posts' do
-        @art_post = ArtPost.create(params)
+        @art_post = ArtPost.new(params)
+        @art_post.seller_id = session[:seller_id]
+        @art_post.save 
         redirect "/art_posts/#{@art_post.id}"
     end
 
     get '/art_posts/:id' do 
-        @art_post = ArtPost.find(params[:id])
+        get_art_post
         erb :'art_posts/show'
     end
 
     get '/art_posts/:id/edit' do 
-        @art_post = ArtPost.find(params[:id])
+        get_art_post
+        redirect_if_not_authorized
         erb :'art_posts/edit'
     end
 
     patch '/art_posts/:id' do 
-        @art_post = ArtPost.find(params[:id])
+        get_art_post
+        redirect_if_not_authorized
         @art_post.update(title: params[:title], description: params[:description], price: params[:price].to_i)
         redirect "/art_posts/#{@art_post.id}"
     end
 
     delete '/art_posts/:id' do
-        @art_post = ArtPost.find(params[:id])
+        get_art_post
+        redirect_if_not_authorized
         @art_post.destroy
         redirect "/art_posts"
+    end
+
+private
+
+    def get_art_post
+        @art_post = ArtPost.find_by_id(params[:id])
+    end
+
+    def redirect_if_not_authorized
+        if @art_post.seller != current_user
+            flash[:error] = "You do not have permission to make changes"
+            redirect '/art_posts'
+        end
     end
 end
