a1c9f3246f926486eef1bbf854209571b64d49e9
David Rafe <david.rafe@gmail.com>
2021-03-01 18:21:15 -0500

    updated artposts controller so buyers cannot create or edit art

--- app/controllers/art_posts_controller.rb
+++ app/controllers/art_posts_controller.rb
@@ -10,15 +10,18 @@ class ArtPostsController < ApplicationController
     end
 
     get '/art_posts/new' do
-        if logged_in? 
-            erb :'art_posts/new'
-        else
+        if !logged_in?
             flash[:error] = "Sorry you have to be logged in to do that"
             redirect '/'
+        elsif current_buyer
+            redirect_if_not_authorized_buyer
+        else
+            erb :'art_posts/new'
         end
     end
 
     post '/art_posts' do
+        redirect_if_not_authorized_buyer
         @art_post = ArtPost.new(params)
         @art_post.seller_id = session[:seller_id]
         @art_post.save 
@@ -32,20 +35,20 @@ class ArtPostsController < ApplicationController
 
     get '/art_posts/:id/edit' do 
         get_art_post
-        redirect_if_not_authorized
+        redirect_if_not_authorized_seller
         erb :'art_posts/edit'
     end
 
     patch '/art_posts/:id' do 
         get_art_post
-        redirect_if_not_authorized
+        redirect_if_not_authorized_seller
         @art_post.update(title: params[:title], img_url: params[:img_url], description: params[:description], price: params[:price].to_i)
         redirect "/art_posts/#{@art_post.id}"
     end
 
     delete '/art_posts/:id' do
         get_art_post
-        redirect_if_not_authorized
+        redirect_if_not_authorized_seller 
         @art_post.destroy
         redirect "/art_posts"
     end
@@ -56,8 +59,15 @@ private
         @art_post = ArtPost.find_by_id(params[:id])
     end
 
-    def redirect_if_not_authorized
+    def redirect_if_not_authorized_seller
         if @art_post.seller != current_seller 
+            flash[:error] = "You do not have permission to make changes"
+            redirect '/art_posts'
+        end
+    end
+
+    def redirect_if_not_authorized_buyer
+        if current_buyer  
             flash[:error] = "You do not have permission to make changes"
             redirect '/art_posts'
         end
